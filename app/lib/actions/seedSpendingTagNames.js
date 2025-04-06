43c83c981e2db4832b07a2e3ce2c59df2de60eac
bradmallett <bmallett2@gmail.com>
2025-04-06 08:17:37 -0500

    spendingTags fixed

--- app/lib/actions/seedSpendingTagNames.js
+++ app/lib/actions/seedSpendingTagNames.js
@@ -10,22 +10,24 @@ export default async function seedSpendingTagNames() {
     const requiredTags = ["food", "home", "car", "restaurant"];
 
     try {
-        const spendingTagNames = await sql `
-            SELECT name FROM spending_tag_names
-            WHERE name IN (${sql(requiredTags)})
-            ;`
-        ;
+        const spendingTagNames = await sql`
+        SELECT name FROM spending_tag_names
+        WHERE name = ANY(${requiredTags}::text[])
+    `;
 
-        const existingTagNames = spendingTagNames.map(tag => tag.name);
-        const missingTags = requiredTags.filter(tag => !existingTagNames.includes(tag));
+        const tagsFromDB = spendingTagNames.map(tag => tag.name);
+        const missingTags = requiredTags.filter(tag => !tagsFromDB.includes(tag));
 
-        for(const tag of missingTags) {
-            await sql `
-                INSERT INTO spending_tag_names (name)
-                VALUES (${tag})
-                ;`
-            ;
+        if(missingTags.length > 0) {
+            for(const tag of missingTags) {
+                await sql `
+                    INSERT INTO spending_tag_names (name)
+                    VALUES (${tag})
+                    ;`
+                ;
+            }
         }
+        
     } catch (error) {
         console.error("Error inserting spending tag names:", error);
         return {
