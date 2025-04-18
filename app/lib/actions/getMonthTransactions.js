ccceb0ba9221f4bf7222545de943572207727d09
bradmallett <bmallett2@gmail.com>
2025-04-18 07:38:22 -0500

    db fix

--- app/lib/actions/getMonthTransactions.js
+++ app/lib/actions/getMonthTransactions.js
@@ -6,40 +6,46 @@ const sql = neon(process.env.DATABASE_URL);
 export default async function getMonthTransactions(monthYear, monthNumber) {
 
     if( monthYear && monthNumber ) {
-        const [ month ] = await sql`
-            SELECT
-                months.id,
-                months.beginning_balance
-            FROM
-                months
-            WHERE 
-                months.year = ${monthYear} 
-            AND 
-                months.number = ${monthNumber};
-        `;
-
-        const transactions = await sql`
-            SELECT 
-                transactions.id,
-                transactions.date,
-                transactions.amount,
-                transactions.type,
-                transactions.description,
-                transactions.budget_category
-            FROM 
-                months
-            JOIN 
-                transactions
-            ON 
-                months.id = transactions.month_id
-            WHERE 
-                months.year = ${monthYear} 
-            AND 
-                months.number = ${monthNumber}
-            ORDER BY 
-                transactions.date ASC;
+        try{
+            const [ month ] = await sql`
+                SELECT
+                    months.id,
+                    months.beginning_balance
+                FROM
+                    months
+                WHERE 
+                    months.year = ${monthYear} 
+                AND 
+                    months.number = ${monthNumber};
             `;
 
-        return { month, transactions };
+            const transactions = await sql`
+                SELECT 
+                    transactions.id,
+                    transactions.date,
+                    transactions.amount,
+                    transactions.type,
+                    transactions.description,
+                    transactions.budget_category
+                FROM 
+                    months
+                JOIN 
+                    transactions
+                ON 
+                    months.id = transactions.month_id
+                WHERE 
+                    months.year = ${monthYear} 
+                AND 
+                    months.number = ${monthNumber}
+                ORDER BY 
+                    transactions.date ASC;
+                `;
+
+            return { month, transactions };
+        }
+        catch (error) {
+            console.error(error);
+            throw new Error("Failed to fetch month transactions");
+        }
     }
 };
