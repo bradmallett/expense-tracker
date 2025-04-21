22b9aad2f7c23196b79eccb43eb42fb43e8ef767
bradmallett <bmallett2@gmail.com>
2025-04-21 15:59:15 -0500

    fixing...

--- app/ui/charts/ChartContain.jsx
+++ app/ui/charts/ChartContain.jsx
@@ -2,13 +2,14 @@
 
 // import CategoriesChart from "./CategoriesChart"
 import { getNameOfMonth } from "../../lib/utils";
-import shapeCategoryData from "../../lib/actions/charts/shapeCategoryData";
+// import shapeCategoryData from "../../lib/actions/charts/shapeCategoryData";
 
 
 export default function ChartContain({ selectedMonth, monthTransactionsData}) {
     const monthName = getNameOfMonth(selectedMonth);
-    const categoryChartData = shapeCategoryData(monthTransactionsData);
-    console.log(categoryChartData)
+    console.log(monthTransactionsData)
+    // const categoryChartData = shapeCategoryData(monthTransactionsData);
+
 
     return (
         <div className="m-0 md:m-5 mt-10 md:mt-16 p-1 md:p-3 pb-6 w-full md:w-1/2 h-[80vw] md:h-[40vw] max-h-[600px] bg-slate-900 text-center text-slate-300">
