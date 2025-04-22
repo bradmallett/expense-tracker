41ec4d1698732495a7f6ba49f822e31a2ceedf38
bradmallett <bmallett2@gmail.com>
2025-04-22 10:09:35 -0500

    trying chart

--- app/ui/charts/ChartContain.jsx
+++ app/ui/charts/ChartContain.jsx
@@ -1,20 +1,19 @@
 
 
-// import CategoriesChart from "./CategoriesChart"
+import CategoriesChart from "./CategoriesChart"
 import { getNameOfMonth } from "../../lib/utils";
 import shapeCategoryData from "../../lib/actions/charts/shapeCategoryData";
 
 
 export default function ChartContain({ selectedMonth, monthTransactionsData}) {
     const monthName = getNameOfMonth(selectedMonth);
-    // const categoryChartData = shapeCategoryData(monthTransactionsData);
-    shapeCategoryData(monthTransactionsData);
+    const categoryChartData = shapeCategoryData(monthTransactionsData);
 
     return (
         <div className="m-0 md:m-5 mt-10 md:mt-16 p-1 md:p-3 pb-6 w-full md:w-1/2 h-[80vw] md:h-[40vw] max-h-[600px] bg-slate-900 text-center text-slate-300">
             <h2 className="font-bold text-xl text-purple-600">BUDGET GOALS</h2>
             <p>PERCENTAGES OF SPENT {monthName} INCOME</p>
-            {/* <CategoriesChart categoryChartData={categoryChartData} /> */}
+            <CategoriesChart categoryChartData={categoryChartData} />
         </div>
     )
 }
