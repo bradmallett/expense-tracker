87557aee9c1c9c8209fdf38b2aa2f736da893ed7
bradmallett <bmallett2@gmail.com>
2025-04-22 09:45:38 -0500

    logging isolated

--- app/lib/actions/charts/shapeCategoryData.js
+++ app/lib/actions/charts/shapeCategoryData.js
@@ -1,49 +1,51 @@
 
 export default function shapeCategoryData(monthTransactionsData) {
-    let totalIncome = 0;
-    let totalFundamental = 0;
-    let totalFun = 0;
-    let totalFuture = 0;
+    console.log('###shapeCategoryData called###: ', monthTransactionsData);
 
-    for(const transaction of monthTransactionsData.transactions) {
-        if(transaction.type === 'income') {
-            totalIncome += transaction.amount;
-        }
+    // let totalIncome = 0;
+    // let totalFundamental = 0;
+    // let totalFun = 0;
+    // let totalFuture = 0;
 
-        if(transaction.type === 'expense') {
-            if(transaction.budget_category === 'fundamental') {
-                totalFundamental += transaction.amount;
-            }
-            if(transaction.budget_category === 'fun') {
-                totalFun += transaction.amount;
-            }
-            if(transaction.budget_category === 'future') {
-                totalFuture += transaction.amount;
-            }
-        }
+    // for(const transaction of monthTransactionsData.transactions) {
+    //     if(transaction.type === 'income') {
+    //         totalIncome += transaction.amount;
+    //     }
 
-        if(transaction.type === 'savings') {
-            totalFuture += transaction.amount;
-        }
-    }
+    //     if(transaction.type === 'expense') {
+    //         if(transaction.budget_category === 'fundamental') {
+    //             totalFundamental += transaction.amount;
+    //         }
+    //         if(transaction.budget_category === 'fun') {
+    //             totalFun += transaction.amount;
+    //         }
+    //         if(transaction.budget_category === 'future') {
+    //             totalFuture += transaction.amount;
+    //         }
+    //     }
 
-    const data = [
-        {
-            name: 'FUNDAMENTAL',
-            target: 50,
-            actual: Number(((totalFundamental / totalIncome) * 100).toFixed(1)),
-        },
-        {
-            name: 'FUN',
-            target: 30,
-            actual: Number(((totalFun / totalIncome) * 100).toFixed(1)),
-        },
-        {
-            name: 'FUTURE',
-            target: 20,
-            actual: Number(((totalFuture / totalIncome) * 100).toFixed(1)),
-        }
-    ];
+    //     if(transaction.type === 'savings') {
+    //         totalFuture += transaction.amount;
+    //     }
+    // }
 
-    return data;
+    // const data = [
+    //     {
+    //         name: 'FUNDAMENTAL',
+    //         target: 50,
+    //         actual: Number(((totalFundamental / totalIncome) * 100).toFixed(1)),
+    //     },
+    //     {
+    //         name: 'FUN',
+    //         target: 30,
+    //         actual: Number(((totalFun / totalIncome) * 100).toFixed(1)),
+    //     },
+    //     {
+    //         name: 'FUTURE',
+    //         target: 20,
+    //         actual: Number(((totalFuture / totalIncome) * 100).toFixed(1)),
+    //     }
+    // ];
+
+    // return data;
 }
