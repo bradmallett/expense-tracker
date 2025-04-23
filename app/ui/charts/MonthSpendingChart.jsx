b82dec8204523a4bf9d86c8268acd6e1197824b6
bradmallett <bmallett2@gmail.com>
2025-04-23 15:30:26 -0500

    working on spendingTagChart

--- app/ui/charts/MonthSpendingChart.jsx
+++ app/ui/charts/MonthSpendingChart.jsx
@@ -1,39 +1,98 @@
 'use client'
-import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
-import { formatNumberToPercent } from '../../lib/utils.js';
+import { PieChart, Pie, Sector, ResponsiveContainer } from 'recharts';
+import { formatCentsToDollars } from '../../lib/utils.js';
+import { useState } from 'react';
 
 
-
-export default function CategoriesChart({ categoryChartData }) {
-
-    const CustomTooltip = ({ active, payload, label }) => {
-        if (!active || !payload || !payload.length) return null;
-      
-        return (
-          <div className="bg-slate-950 p-2 text-xs border-2 border-purple-600">
-            <p className='p-1 text-sm text-purple-600 font-bold'>{label}</p>
-            <p className='pb-1'>{`GOAL: ${payload[0].value}%`}</p>
-            <p>{`ACTUAL: ${payload[1].value}%`}</p>
-          </div>
-        );
-      };
-
-
+const CategoriesChart = ({ shapedMonthTagsData }) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+  
+    const onPieEnter = (_, index) => {
+      setActiveIndex(index);
+    };
+  
+    const renderActiveShape = (props) => {
+      const RADIAN = Math.PI / 180;
+      const {
+        cx,
+        cy,
+        midAngle,
+        innerRadius,
+        outerRadius,
+        startAngle,
+        endAngle,
+        fill,
+        payload,
+        percent,
+        value,
+      } = props;
+      const sin = Math.sin(-RADIAN * midAngle);
+      const cos = Math.cos(-RADIAN * midAngle);
+      const sx = cx + (outerRadius + 10) * cos;
+      const sy = cy + (outerRadius + 10) * sin;
+      const mx = cx + (outerRadius + 30) * cos;
+      const my = cy + (outerRadius + 30) * sin;
+      const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+      const ey = my;
+      const textAnchor = cos >= 0 ? 'start' : 'end';
+  
+      return (
+        <g >
+          <text x={cx} y={cy} dy={0} textAnchor="middle" fill={payload.tagColor} className='font-bold'>
+            {payload.tagName}
+          </text>
+          <text x={cx} y={cy} dy={25} textAnchor="middle" fill={payload.tagColor}>
+            {formatCentsToDollars(payload.tagAmount)}
+          </text>
+          <Sector
+            cx={cx}
+            cy={cy}
+            innerRadius={innerRadius}
+            outerRadius={outerRadius}
+            startAngle={startAngle}
+            endAngle={endAngle}
+            fill={payload.tagColor}
+          />
+          <Sector
+            cx={cx}
+            cy={cy}
+            startAngle={startAngle}
+            endAngle={endAngle}
+            innerRadius={outerRadius + 6}
+            outerRadius={outerRadius + 15}
+            fill={payload.tagColor}
+          />
+          {/* <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={payload.tagColor} fill="none" /> */}
+          {/* <circle cx={ex} cy={ey} r={2} fill={payload.tagColor} stroke="none" /> */}
+          {/* <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill={payload.tagColor}>{`Spent: ${value}`}</text> */}
+          
+        </g>
+      );
+    };
+  
     return (
-        <>
-            <ResponsiveContainer width="100%" height="100%">
-                <BarChart width={150} height={40} data={categoryChartData} className='font-bold pr-4 pb-6'>
-                    <CartesianGrid strokeDasharray="5 5" vertical={false} stroke="#9333ea"/>
-                    <YAxis stroke="#9333ea" className='text-xs' tickFormatter={formatNumberToPercent}/>
-                    <Bar dataKey="target" fill="#581c8796" strokeWidth="3px" barSize={100} xAxisId="one"/>
-                    <XAxis dataKey="name" stroke="#9333ea" xAxisId="one" />
-                    <Bar dataKey="actual" fill="#9333ea" barSize={70} xAxisId="two" activeBar={{ fill: '#9333ea', stroke: 'none', style: {filter: 'brightness(1.2)',},}} />
-                    <XAxis dataKey="name" xAxisId="two" hide/>
-                    <Tooltip content={CustomTooltip} cursor={false}/>
-                </BarChart>
-            </ResponsiveContainer>
-        </>
-    )
-
-}
+      <ResponsiveContainer width="100%" height="100%" >
+        <PieChart>
+          <Pie
+            activeIndex={activeIndex}
+            activeShape={renderActiveShape}
+            data={shapedMonthTagsData}
+            cx="50%"
+            cy="50%"
+            innerRadius={150}
+            outerRadius={200}
+            fill='#334155'
+            paddingAngle={2}
+            stroke='none'
+            dataKey="tagAmount" 
+            nameKey="tagName"
+            onMouseEnter={onPieEnter}
+            
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    );
+  };
+  
+  export default CategoriesChart;
 
--- app/ui/charts/SpendingTagsChartContain.jsx
+++ app/ui/charts/SpendingTagsChartContain.jsx
@@ -1,15 +1,17 @@
 import { getNameOfMonth } from "../../lib/utils";
-// import shapeMonthSpendingTagData from "../../lib/actions/charts/shapeMonthSpendingTagData";
+import shapeMonthSpendingTagData from "../../lib/actions/charts/shapeMonthSpendingTagData";
+import MonthSpendingChart from "./MonthSpendingChart";
 
 export default function SpendingTagsChartContain({ selectedMonth, monthTransactionsData, spendingTagInstances}) {
     const monthName = getNameOfMonth(selectedMonth);
-    // shapeMonthSpendingTagData(monthTransactionsData, spendingTagInstances);
+    const shapedMonthTagsData = shapeMonthSpendingTagData(monthTransactionsData, spendingTagInstances);
+    
 
     return (
-        <div className="m-0 md:m-5 mt-10 md:mt-16 p-1 md:p-3 pb-6 w-full h-[80vw] md:h-[30vw] max-h-[600px] bg-slate-900 text-center text-slate-300">
+        <div className="m-0 md:m-5 mt-10 md:mt-16 p-1 md:p-3 pb-6 w-full h-[80vw] md:h-[40vw] max-h-[600px] bg-slate-900 text-center text-slate-300">
             <h2 className="font-bold text-xl text-purple-600">{monthName} SPENDING</h2>
-            {/* <MonthSpendingChart  />
-            <YearSpendingChart  /> */}
+            <MonthSpendingChart shapedMonthTagsData={shapedMonthTagsData}/>
+            {/* <YearSpendingChart  /> */}
         </div>
     )
 }
