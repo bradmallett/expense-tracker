7eb12cc4ea1ab3776de8c8a1f86aaa212e0d2d7d
bradmallett <bmallett2@gmail.com>
2025-04-25 09:44:41 -0500

    styling totals component

--- app/ui/Totals.jsx
+++ app/ui/Totals.jsx
@@ -2,6 +2,7 @@ import getTotals from "../lib/actions/getTotals";
 import { ArrowDownIcon, ArrowUpIcon} from '@heroicons/react/24/outline';
 import clsx from "clsx";
 import { formatCentsToDollars } from "../lib/utils";
+import Triangle from "./Triangle";
 
 
 export async function Totals({ selectedMonth }) {
@@ -31,40 +32,110 @@ export async function Totals({ selectedMonth }) {
 
 
         return (
-            <div className="mt-2 md:p-2 md:w-11/12 flex justify-around text-xs border-b border-orange-600 max-w-[1480px] mx-auto">
-                <div className="text-center w-1/3 flex justify-evenly">
-                    <div className="text-center">
-                        <p className="text-green-500">{`${monthName} Income`}</p>
-                        <p>{`${formatCentsToDollars(totalMonthIncome)}`}</p>
+            <div className="w-100 pt-5 pb-5 bg-slate-800">
+                <div className="w-100 max-w-[1200px] mx-auto flex justify-evenly">
+
+
+                <div className="flex w-1/3 justify-center">
+                    <div className="flex flex-col items-center">
+                        <Triangle direction={'up'} size={'w-5 h-5' } color={'text-green-600'} />
+                        <p className="text-xs text-green-600 font-bold">{`${formatCentsToDollars(totalMonthIncome)}`}</p>
                     </div>
-                    <div className="text-center">
-                        <p className="text-red-500">{`${monthName} Spent`}</p>
-                        <p>{`${formatCentsToDollars(totalMonthExpense)}`}</p>
+                    <p className="font-bold text-sm">{monthName}</p>
+                    <div className="flex flex-col items-center">
+                        <Triangle direction={'down'} size={'w-5 h-5' } color={'text-red-600'} />
+                        <p className="text-xs text-red-600 font-bold">{`${formatCentsToDollars(totalMonthExpense)}`}</p>
                     </div>
                 </div>
-                <div className="text-center w-1/3">
-                    <div className="flex justify-center">
-                        <p>{`${monthName}`}</p>
-                        <ArrowUpIcon className="size-4 text-green-500" />
-                        <ArrowDownIcon className="size-4 text-red-500" />
+
+
+
+
+                <div className="w-[3px] h-[30px] bg-slate-300 rounded-sm md:hidden"></div>
+
+                <div className="hidden md:block text-center w-1/3 font-bold text-sm md:show">
+                   <div className="flex justify-center">
+                     <p>{`${monthName}`}</p>
+                         <ArrowUpIcon className="size-4 text-green-500 font-bold" />
+                         <ArrowDownIcon className="size-4 text-red-500 font-bold" />
+                     </div>
+                     <p className={clsx(monthTotalDifference > 0 ? "text-green-600" : "text-red-600")}
+                     >
+                         {`${formatCentsToDollars(monthTotalDifference)}`}
+                         </p>
+                 </div>
+
+                
+
+
+                <div className="flex w-1/3 justify-center">
+                    <div className="flex flex-col items-center">
+                        <Triangle direction={'up'} size={'w-5 h-5' } color={'text-green-600'}/>
+                        <p className="text-xs text-green-600 font-bold">{`${formatCentsToDollars(totalYearIncome)}`}</p>
                     </div>
-                    <p className={clsx(monthTotalDifference > 0 ? "text-green-500" : "text-red-500",
-                        "text-base font-bold")}
-                    >
-                        {`${formatCentsToDollars(monthTotalDifference)}`}
-                        </p>
+                    <p className="font-bold text-sm">{year}</p>
+                    <div className="flex flex-col items-center">
+                        <Triangle direction={'down'} size={'w-5 h-5' } color={'text-red-600'}/>
+                        <p className="text-xs text-red-600 font-bold">{`${formatCentsToDollars(totalYearExpense)}`}</p>
+                    </div>
                 </div>
-                <div className="text-center w-1/3 flex justify-evenly">
-                    <div className="text-center">
-                        <p className="text-red-500">{`${year} Spent`}</p>
-                        <p>{`${formatCentsToDollars(totalYearExpense)}`}</p>
-                    </div>
-                    <div className="text-center">
-                        <p className="text-green-500">{`${year} Income`}</p>
-                        <p>{`${formatCentsToDollars(totalYearIncome)}`}</p>
-                    </div>
+
+
                 </div>
             </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // <div className="mt-2 md:p-2 md:w-11/12 flex justify-around text-xs border-b border-orange-600 max-w-[1480px] mx-auto">
+            //     <div className="text-center w-1/3 flex justify-evenly">
+            //         <div className="text-center">
+            //             <p className="text-green-500">{`${monthName} Income`}</p>
+            //             <p>{`${formatCentsToDollars(totalMonthIncome)}`}</p>
+            //         </div>
+            //         <div className="text-center">
+            //             <p className="text-red-500">{`${monthName} Spent`}</p>
+            //             <p>{`${formatCentsToDollars(totalMonthExpense)}`}</p>
+            //         </div>
+            //     </div>
+            //     <div className="text-center w-1/3">
+            //         <div className="flex justify-center">
+            //             <p>{`${monthName}`}</p>
+            //             <ArrowUpIcon className="size-4 text-green-500" />
+            //             <ArrowDownIcon className="size-4 text-red-500" />
+            //         </div>
+            //         <p className={clsx(monthTotalDifference > 0 ? "text-green-500" : "text-red-500",
+            //             "text-base font-bold")}
+            //         >
+            //             {`${formatCentsToDollars(monthTotalDifference)}`}
+            //             </p>
+            //     </div>
+            //     <div className="text-center w-1/3 flex justify-evenly">
+            //         <div className="text-center">
+            //             <p className="text-red-500">{`${year} Spent`}</p>
+            //             <p>{`${formatCentsToDollars(totalYearExpense)}`}</p>
+            //         </div>
+            //         <div className="text-center">
+            //             <p className="text-green-500">{`${year} Income`}</p>
+            //             <p>{`${formatCentsToDollars(totalYearIncome)}`}</p>
+            //         </div>
+            //     </div>
+            // </div>
         )
     
 }
